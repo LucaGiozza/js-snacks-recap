d5fd72c22734fb4f312a0768680f6af9690ca39d
Luca Giozza <luca.giozza@gmail.com>
2021-06-18 17:10:06 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -5,21 +5,18 @@
 //  che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
 
-// function miaFunction(array,a ,b){
 
-// }
 const numeri = [6,8,15,21,24,28,34];
 
 let a = 8;
 let b = 28;
 
 
-const array = [];
+
 // mi deve restituire i valori compresi tra 8 e 28 compresi
- let numeriTra = numeri.filter((element) => element > 6 );
- numeriTra = numeri.filter((element) => element < 34  );
-array.push(numeriTra);
-console.log(array)
+ let numeriTra = numeri.filter((element) => element > 6 && element < 34  );
+
+console.log(numeriTra);
 
 
 
