792a892494ceccd3dd189788cfd0051ef639b8b5
Luca Giozza <luca.giozza@gmail.com>
2021-06-18 15:52:01 +0200

    continuazione prova es

--- js/script.js
+++ js/script.js
@@ -12,10 +12,14 @@ const numeri = [6,8,15,21,24,28,34];
 
 let a = 8;
 let b = 28;
+
+
+const array = [];
 // mi deve restituire i valori compresi tra 8 e 28 compresi
-let numeriTra = numeri.filter((element) => element > 6  );
+ let numeriTra = numeri.filter((element) => element > 6 );
  numeriTra = numeri.filter((element) => element < 34  );
-console.log(numeriTra);
+array.push(numeriTra);
+console.log(array)
 
 
 
